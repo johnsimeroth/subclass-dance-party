97386ee8a308272a3e0aa8a7a016b02b459173d4
Johnathan Simeroth <john.simeroth@gmail.com>
2023-02-13 17:37:21 -0800

    add fading dancer

---
+++ src/fadingDancer.js
@@ -0,0 +1,18 @@
+var MakeFadingDancer = function(top, left, timeBetweenSteps) {
+
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.timeBetweenSteps = timeBetweenSteps;
+};
+
+MakeFadingDancer.prototype = Object.create(MakeDancer.prototype);
+MakeFadingDancer.prototype.constructor = MakeFadingDancer;
+
+MakeFadingDancer.prototype.step = function() {
+  MakeDancer.prototype.oldStep.call(this);
+  this.$node.fadeToggle(this.timeBetweenSteps);
+};
+
+
+// toggle() is a jQuery method to show/hide the <span> tag.
+// See http://api.jquery.com/category/effects/ for this and
+// other effects you can use on a jQuery-wrapped html tag.
